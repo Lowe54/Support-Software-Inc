301bcb5e5f912fd9a632d9e292c32a46df89ed5a
James Lowe <35152800+Lowe54@users.noreply.github.com>
2019-12-14 22:32:09 +0000

    Core Module - views.py - index function added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    Function that returns the index page
+    """
+    return render(request, 'index.html')
