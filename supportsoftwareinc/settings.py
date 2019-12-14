103c0f48e9d624a2b9a9f6ed0924944cdb8717dc
James Lowe <35152800+Lowe54@users.noreply.github.com>
2019-12-14 22:47:04 +0000

    Application settings file updated to include 'templates' folder

--- supportsoftwareinc/settings.py
+++ supportsoftwareinc/settings.py
@@ -54,8 +54,11 @@ ROOT_URLCONF = 'supportsoftwareinc.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+			os.path.join(BASE_DIR, 'templates')
+		],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
