4dfc5afa7ff8d4d74fc785dd01e5d4591a6ee160
James Lowe <35152800+Lowe54@users.noreply.github.com>
2019-12-31 19:32:47 +0000

    Sidebar functionality - Fixed issue with main content not being pushed to the left when sidebar is opened

--- static/js/core.js
+++ static/js/core.js
@@ -3,17 +3,17 @@ $('#toggle_sidebar').on('click', function(){
 
     if (sidebar.hasClass('closed')){
         $(sidebar).removeClass('closed');
-        $(sidebar).addClass('transition-open');
+        $(sidebar).addClass('transition');
         setTimeout(function(){
-                $(sidebar).removeClass('transition-open');
+                $(sidebar).removeClass('transition');
                 $(sidebar).addClass('open'); 
         }, 100);
     }
     else{
         $(sidebar).removeClass('open');
-        $(sidebar).addClass('transition-close'); 
+        $(sidebar).addClass('transition'); 
         setTimeout(function(){
-                $(sidebar).removeClass('transition-close');
+                $(sidebar).removeClass('transition');
                 $(sidebar).addClass('closed'); 
         }, 500);                     
     }
