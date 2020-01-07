035d66d6cb53e58709a2cd70a466d0113c809aab
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-07 19:46:59 +0000

    Authentication - models.py Organisation docstring added plus formatting for readability

--- authentication/models.py
+++ authentication/models.py
@@ -2,7 +2,19 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class Organisation(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.TextField(max_length=255, name="Organisation Name")
+    '''
+    Organisation Table
+
+    id - Organisations ID
+    name - Organisations Name (Max length of 255 characters)
+    '''
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+        )
+    name = models.TextField(
+        max_length=255,
+        name="Organisation Name"
+        )
