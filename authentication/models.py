ea6e255b39a544f6b6b82ee95d3ac594f7d8d3f1
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-07 20:36:30 +0000

    Authentication - models.py - Changed User to MyUser

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,6 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
 class Organisation(models.Model):
     '''
@@ -19,12 +19,16 @@ class Organisation(models.Model):
         name="Organisation Name"
         )
 
-class User(AbstractUser):
+class MyUser(models.Model):
     '''
     User Table - Extends the base User model via Abstract User
     profile_picture - Image Field that holds the user's profile image
     organisation - One to Many relationship - Organisation table id
     '''
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+        )
     profile_picture = models.ImageField(
         name="Profile Picture",
         upload_to=''
@@ -33,4 +37,3 @@ class User(AbstractUser):
         Organisation,
         on_delete=models.CASCADE
         )
-        
