a7e6af6bab46a52c88323702ef93b0d8ae91cf0c
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-09 18:45:54 +0000

    Authentication - admin.py - Registered custom user model as inline tabular

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import MyUser, Organisation
+from django.contrib.auth.models import User
 
+from .models import MyUser
 # Register your models here.
+class User_Inline(admin.TabularInline):
+    model = MyUser
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [
+        User_Inline
+    ]
+
+# Since we are extending the default user, we first need to
+# deregister the default User model
+admin.site.unregister(User)
+
+# Then re-register them both
+admin.site.register(User, UserAdmin)
+admin.site.register(Organisation)
