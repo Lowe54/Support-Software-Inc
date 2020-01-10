abaa417cab457ed24ed3e2275321bbe198a6dd4c
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-10 18:29:25 +0000

    Authentication - Login view created

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
 # Create your views here.
+def login(request):
+    login_form = LoginForm()
+
+    return render(request, 'login.html', {'loginForm' : login_form})
