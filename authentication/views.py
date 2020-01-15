02b11e22e846577970ea227b3ae10b6c15a43838
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-15 21:31:00 +0000

    Authentication - login view updated with authentication functionality, plus already logged in check

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
+from django.contrib import messages
 from .forms import LoginForm
 # Create your views here.
 def login(request):
-    login_form = LoginForm()
+    '''
+    Login functionality
+    '''
+    # If user is already logged in, redirect to index with a page
+    if request.user.is_authenticated:
+        messages.add_message(request, messages.INFO, "You are already signed in")
+        return redirect('/')
+
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            user = auth.authenticate(
+                username=request.POST['username'],
+                password=request.POST['password']
+            )
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in")
+                return redirect(reverse('index'))
+        
+            login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = LoginForm()
 
     return render(request, 'login.html', {'loginForm' : login_form})
