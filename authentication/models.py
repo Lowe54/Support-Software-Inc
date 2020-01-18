68b8dcaf61ee83e8d5eb2b35ba14d15b54eda4f2
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 12:32:14 +0000

    Role field added to user model

--- authentication/models.py
+++ authentication/models.py
@@ -2,6 +2,7 @@ import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Organisation(models.Model):
     '''
     Organisation Table
@@ -18,11 +19,13 @@ class Organisation(models.Model):
         max_length=255,
         name="Organisation_Name"
         )
+
     def __str__(self):
         '''
         Returns the Organisation's Name
         '''
         return self.Organisation_Name
+
 
 class MyUser(models.Model):
     '''
@@ -30,10 +33,19 @@ class MyUser(models.Model):
     profile_picture - Image Field that holds the user's profile image
     organisation - One to Many relationship - Organisation table id
     '''
+    ROLE_CHOICES = [
+        ('AGN', 'Agent'),
+        ('USR', 'End User')
+    ]
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
         )
+    role = models.CharField(
+        max_length=3,
+        choices=ROLE_CHOICES,
+        default="USR",
+    )
     profile_picture = models.ImageField(
         name="Profile Picture",
         upload_to='',
