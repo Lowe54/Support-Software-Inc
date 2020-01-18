2f3ffbedfdc5986baeee3417292ca9cf7c2775cc
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 12:26:57 +0000

    Ticket Model - Ticket table initial commit

--- ticket/models.py
+++ ticket/models.py
@@ -1,3 +1,74 @@
+import uuid
 from django.db import models
 
+from authentication.models import User as Users
 # Create your models here.
+
+
+class Ticket(models.Model):
+    '''
+    Table to hold core ticket information.
+    '''
+    PRIORITY_CHOICES = [
+        ('LOW', 'Low'),
+        ('MED', 'Medium'),
+        ('HGH', 'High'),
+    ]
+    STATUS_CHOICES = [
+        ('OPN', 'Open'),
+        ('PEN', 'Pending'),
+        ('ONH', 'On Hold'),
+        ('CLS', 'Closed'),
+    ]
+    id = models.UUIDField(
+        primary_key=True,
+        db_index=True,
+        default=uuid.uuid4,
+        editable=False
+    )
+    title = models.CharField(
+        max_length=50,
+        null=False,
+        blank=False,
+    )
+    description = models.TextField(
+        null=False,
+        blank=False,
+    )
+    status = models.CharField(
+        max_length=3,
+        choices=STATUS_CHOICES,
+        null=False,
+        blank=False,
+        default='NEW'
+    )
+    raised_on = models.DateTimeField(
+        auto_now=True,
+        name="Raised On",
+    )
+    raised_by = models.ForeignKey(
+        to=Users,
+        on_delete=models.CASCADE,
+        related_name='+'
+    )
+    associated_users = models.ManyToManyField(
+        to=Users,
+        related_name='+',
+        blank=True,
+    )
+    priority = models.CharField(
+        max_length=3,
+        choices=PRIORITY_CHOICES,
+        null=False,
+        blank=False
+    )
+    closed_on = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+    closure_message = models.CharField(
+        max_length=150,
+        null=True,
+        blank=True,
+
+    )
