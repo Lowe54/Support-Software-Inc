93bbfe482640e16735040cb1999662607e3be07f
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 13:13:37 +0000

    Authentication URLS - Added logout url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,9 @@
+'''
+URL File for Authentication module
+'''
 from django.urls import path
-from .views import login
+from .views import login, logout
 urlpatterns = [
-    path(r'login', login, name="login")
+    path(r'login', login, name="login"),
+    path(r'logout', logout, name="logout")
 ]
