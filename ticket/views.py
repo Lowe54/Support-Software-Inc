e24ec14fd75d75a9a3ce48cb241aa677ad47d8ba
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 15:29:32 +0000

    Ticket views initial commit

--- ticket/views.py
+++ ticket/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+'''
+Views.py file for Ticket Module
 
-# Create your views here.
+dashboard
+
+Ticket_List
+Ticket_View
+
+Edit_Ticket
+'''
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+from authentication.models import MyUser
+
+
+@login_required
+def dashboard(request):
+    '''
+    View that renders the appropriate dashboard that is visible when
+    they sign in
+    '''
+    if request.user.is_authenticated:
+        custom_user = MyUser.objects.get(user_id=request.user.id)
+        if custom_user.role != 'AGN':
+            return redirect('/')
+        return render(request, 'agent_dashboard.html', {'user': custom_user})
+    return redirect('login')
