32bf68f252f1c27c6e271d0009db4db2b5230816
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 16:03:45 +0000

    Dashboard view updated with count queries

--- ticket/views.py
+++ ticket/views.py
@@ -12,6 +12,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from authentication.models import MyUser
+from .models import Ticket
 
 
 @login_required
@@ -24,5 +25,29 @@ def dashboard(request):
         custom_user = MyUser.objects.get(user_id=request.user.id)
         if custom_user.role != 'AGN':
             return redirect('/')
-        return render(request, 'dashboard.html', {'user': custom_user})
+        # Try to get a count for each of the 3 counters
+        try:
+            unassigned_tickets = Ticket.objects.get(assigned_to=None)
+            assigned_tickets = Ticket.objects.get(
+                assigned_to=custom_user.user_id
+                )
+            open_tickets = Ticket.objects.get(
+                status='OPN'
+            )
+        # Catch any DoesNotExist errors, which means the count would be 0
+        except Ticket.DoesNotExist:
+            unassigned_tickets = 0
+            assigned_tickets = 0
+            open_tickets = 0
+
+        return render(
+            request,
+            'dashboard.html',
+            {
+                'user': custom_user,
+                'unassigned': unassigned_tickets,
+                'assigned': assigned_tickets,
+                'open_tickets': open_tickets
+            }
+        )
     return redirect('login')
