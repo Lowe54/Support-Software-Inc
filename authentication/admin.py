50a30a7a623e92a9397a75988f04608d23a2656a
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 17:08:56 +0000

    Authentication admin.py - module docstring added

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,6 @@
+'''
+Admin.py file for Authentication module
+'''
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin as UA
