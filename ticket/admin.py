d606eaf9b249a62926b7e1ffcdb0be11b58e26ba
Lowe54 <35152800+Lowe54@users.noreply.github.com>
2020-01-18 20:59:23 +0000

    Customised admin table for Ticket module

--- ticket/admin.py
+++ ticket/admin.py
@@ -4,5 +4,9 @@ Admin.py file for Ticket Module
 from django.contrib import admin
 from .models import Ticket
 
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'raised_on', 'status')
 
-admin.site.register(Ticket)
+admin.site.register(Ticket, TicketAdmin)
+
+
