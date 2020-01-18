b81db200076e545b8352761d358bd9c2a5ccf8bf
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-18 17:33:28 +0000

    Results page added to ticket url file

--- ticket/urls.py
+++ ticket/urls.py
@@ -3,7 +3,8 @@ Urls.py file for ticket module
 '''
 
 from django.urls import path
-from .views import dashboard
+from .views import dashboard, results
 urlpatterns = [
-    path(r'/dashboard', dashboard, name="dashboard")
+    path(r'dashboard', dashboard, name="dashboard"),
+    path(r'results', results, name="results")
 ]
