c70d02bfa3d5fc321d72b096bc61158c48f636e2
Lowe54 <35152800+Lowe54@users.noreply.github.com>
2020-01-18 22:08:23 +0000

    Ticket forms.py initial commit

---
+++ ticket/forms.py
@@ -0,0 +1,27 @@
+'''
+Forms.py file for Ticket module
+'''
+
+from django import forms
+from .models import STATUS_CHOICES, PRIORITY_CHOICES
+
+class FilterForm(forms.Form):
+    '''
+    Form that renders the 'Filters' on the side
+    of the results page
+    '''
+    Keyword = forms.CharField(
+        required=False,
+        help_text="Enter Keyword to search for"   
+    )
+    status = forms.ChoiceField(
+        required=False,
+        choices=STATUS_CHOICES,
+        widget=forms.CheckboxSelectMultiple
+    )
+    priority = forms.ChoiceField(
+        required=False,
+        choices=PRIORITY_CHOICES,
+        widget=forms.CheckboxSelectMultiple
+    )
+    
