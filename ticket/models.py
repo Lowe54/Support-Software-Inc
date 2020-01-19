585bd453058a0fc502c0511a7fc2c11780d6e026
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-19 13:25:07 +0000

    Ticket model updated with updated_on field

--- ticket/models.py
+++ ticket/models.py
@@ -83,6 +83,10 @@ class Ticket(models.Model):
         blank=True,
 
     )
+    updated_on = models.DateField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
