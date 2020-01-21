4e1ca34188f8f77c59485c24affc897ad46a747c
James Lowe <35152800+Lowe54@users.noreply.github.com>
2020-01-21 20:38:36 +0000

    Organisation model moved

--- organisations/models.py
+++ organisations/models.py
@@ -1,3 +1,27 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Organisation(models.Model):
+    '''
+    Organisation Table
+
+    id - Organisations ID
+    name - Organisations Name (Max length of 255 characters)
+    '''
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+        )
+    Organisation_Name = models.CharField(
+        max_length=255,
+        name="Organisation_Name"
+        )
+
+    def __str__(self):
+        '''
+        Returns the Organisation's Name
+        '''
+        return self.Organisation_Name
+        
