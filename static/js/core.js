c6cb61cb539b04f91016274026dd4e15a923d30d
Lowe54 <35152800+Lowe54@users.noreply.github.com>
2020-01-24 20:14:52 +0000

    Core JS updated with initial responsive Class setting

--- static/js/core.js
+++ static/js/core.js
@@ -1,9 +1,15 @@
+$(document).ready(function(){
+    $('#content-wrapper').addClass('col-lg-12');
+});
+
 $('#toggle_sidebar').on('click', function(){
     let sidebar = $('#sidebar-navigation');
 
     if (sidebar.hasClass('closed')){
         $(sidebar).removeClass('closed');
         $(sidebar).addClass('transition');
+        $('#content-wrapper').removeClass('col-lg-12').addClass('col-lg-10'); 
+               
         setTimeout(function(){
                 $(sidebar).removeClass('transition');
                 $(sidebar).addClass('open'); 
@@ -14,7 +20,11 @@ $('#toggle_sidebar').on('click', function(){
         $(sidebar).addClass('transition'); 
         setTimeout(function(){
                 $(sidebar).removeClass('transition');
-                $(sidebar).addClass('closed'); 
-        }, 500);                     
+                $(sidebar).addClass('closed');
+                $('#content-wrapper').removeClass('col-lg-10').addClass('col-lg-12'); 
+        }, 500);
+        
+        
+        
     }
 });
