401c7c59d20c48935a97700cabcbf5a6a1788020
Lowe54 <jameslowe241@gmail.com>
2020-02-09 15:47:31 +0000

    User field now filled if logged in user purchases  the software

--- payments/views.py
+++ payments/views.py
@@ -1,17 +1,17 @@
+'''
+Views.py file for Payments app
+'''
 import os
-
+from datetime import date
 from django.shortcuts import render, HttpResponse
+import stripe
+from authentication.models import MyUser
 from .models import Order
 from .forms import OrderForm
-from datetime import date
-import stripe
 
 
 def render_payment_form(request):
 
-    # Set your secret key: remember to switch to your live secret key in
-    # production
-    # See your keys here: https://dashboard.stripe.com/account/apikeys
     stripe.api_key = os.getenv('STRIPE_SECRET')
 
     intent = stripe.PaymentIntent.create(
@@ -33,17 +33,32 @@ def render_payment_form(request):
 
 def add_order(request):
     if request.method == 'POST':
-        Order.objects.create(
-            full_name=request.POST['full_name'],
-            street_address1=request.POST['street_address_1'],
-            street_address2=request.POST['street_address_2'],
-            town_or_city=request.POST['town_or_city'],
-            county=request.POST['county'],
-            country=request.POST['country'],
-            phone_number = request.POST['phone_number'],
-            date=date.today(),
-            amount=request.POST['amount'],
-            confirmation_code=request.POST['confirmation_code']   
-        )
-        return HttpResponse(status=200)  
+        if request.user:
+            Order.objects.create(
+                user=MyUser.objects.get(user_id=request.user.id),
+                full_name=request.POST['full_name'],
+                street_address1=request.POST['street_address_1'],
+                street_address2=request.POST['street_address_2'],
+                town_or_city=request.POST['town_or_city'],
+                county=request.POST['county'],
+                country=request.POST['country'],
+                phone_number=request.POST['phone_number'],
+                date=date.today(),
+                amount=request.POST['amount'],
+                confirmation_code=request.POST['confirmation_code']
+            )
+        else:
+            Order.objects.create(
+                full_name=request.POST['full_name'],
+                street_address1=request.POST['street_address_1'],
+                street_address2=request.POST['street_address_2'],
+                town_or_city=request.POST['town_or_city'],
+                county=request.POST['county'],
+                country=request.POST['country'],
+                phone_number=request.POST['phone_number'],
+                date=date.today(),
+                amount=request.POST['amount'],
+                confirmation_code=request.POST['confirmation_code']
+            )
+        return HttpResponse(status=200)
     return HttpResponse(status=403)
