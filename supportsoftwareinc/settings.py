5c84299e8c403008c6ca9ee829b23020866589de
Lowe54 <35152800+Lowe54@users.noreply.github.com>
2020-02-10 20:18:10 +0000

    Settings.py file updated

--- supportsoftwareinc/settings.py
+++ supportsoftwareinc/settings.py
@@ -154,14 +154,11 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
         'mail_admins': {
             'level': 'ERROR',
             'class': 'django.utils.log.AdminEmailHandler',
             'include_html': True,
-        }   
+        }  
     },
     'loggers': {
         '': {
