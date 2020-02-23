c92e5d114a548563899ec6ecb890288fa0ed7132
Lowe54 <jameslowe241@gmail.com>
2020-02-23 16:55:29 +0000

    Organisation forms - Custom textbox classes added to formhelper

--- organisations/forms.py
+++ organisations/forms.py
@@ -1,3 +1,4 @@
+from crispy_forms.helper import FormHelper
 from django import forms
 from django.forms import ModelForm
 
@@ -16,6 +17,9 @@ class UnassociatedUserForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(UnassociatedUserForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'custom-control custom-checkbox'
+
         self.fields['users'] = forms.MultipleChoiceField(
             choices=[(o.user_id, o.get_username_and_email())
                      for o in MyUser.objects.filter(organisation_id=None)],
