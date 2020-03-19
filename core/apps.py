4df52d97373e102528819d7dd23cbd1d60bb1aa9
Lowe54 <jameslowe241@gmail.com>
2020-03-19 21:35:56 +0000

    Core module - Missing classStrings / Docstrings added

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,11 @@
+'''
+Apps.py file for core module
+'''
 from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    '''
+    Define the name of the application (core)
+    '''
     name = 'core'
--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,3 @@
-from django.test import TestCase
-
-# Create your tests here.
+'''
+Core module tests.py file
+'''
